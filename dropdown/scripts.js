7a07489a3f0baa0284397f92026d045453f3387e
dangnguyen2k2 <nguyenthidang303@gmail.com>
2025-06-11 21:11:06 +0700

    use keyboard to select menu item in dropdown

--- dropdown/scripts.js
+++ dropdown/scripts.js
@@ -1,6 +1,33 @@
 const toggleButton = document.getElementById("dropdownToggle");
 const menu = document.getElementById("dropdownMenu");
-const options = menu.querySelectorAll('.dropdown-content__item');
+const options = menu.querySelectorAll(".dropdown-content__item");
+const dropdownContainer = document.querySelector(".dropdown-container");
+
+let currentIndex = -1;
+const maxLength = options.length - 1;
+
+const selectItem = (item, index) => {
+  const selected = document.getElementById("selectedItem");
+  selected.textContent = "Menu item được chọn là: " + item.textContent;
+  options.forEach((option) => option.classList.remove("active"));
+  options[index].classList.add("active");
+};
+
+const handleArrow = (e) => {
+  e.preventDefault();
+  if (e.key === "Enter") {
+    selectItem(options[currentIndex], currentIndex);
+    return;
+  } else if (e.key === "ArrowDown") {
+    currentIndex = currentIndex < maxLength ? currentIndex + 1 : maxLength;
+  } else if (e.key === "ArrowUp") {
+    currentIndex = currentIndex > 0 ? currentIndex - 1 : 0;
+  }
+  if (currentIndex !== -1) {
+    options.forEach((option) => option.classList.remove("active"));
+    options[currentIndex].classList.add("active");
+  }
+};
 
 toggleButton.addEventListener("click", () => {
   menu.classList.toggle("show");
@@ -9,23 +36,7 @@ toggleButton.addEventListener("click", () => {
 document.addEventListener("click", (e) => {
   if (!toggleButton.contains(e.target)) {
     menu.classList.remove("show");
+    currentIndex = -1;
   }
 });
-
-const selectedItem = (item) => {
-  const selected = document.getElementById("selectedItem");
-  selected.textContent = "Menu item được chọn là: " + item.textContent;
-};
-
-let currentIndex = -1
-menu.addEventListener('keydown', (e) => {
-    const maxLength = options.length - 1
-    if(e.key==='ArrowDown') {
-        currentIndex = currentIndex < maxLength ? currentIndex + 1 : maxLength
-        options[currentIndex].focus()
-    }
-    else if(e.key==='ArrowUp') {
-        currentIndex = currentIndex > 0 ? currentIndex - 1 : 0
-        options[currentIndex].focus()
-    }
-})
+dropdownContainer.addEventListener("keydown", handleArrow);
